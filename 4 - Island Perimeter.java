f27d9d5b6103b8c05ca221db75f8cfda3c0fbf49
KevzPeter <ultimatekevin1@gmail.com>
2021-10-05 02:22:33 +0530

    Update 4 - Island Perimeter.java

--- 4 - Island Perimeter.java
+++ 4 - Island Perimeter.java
@@ -21,3 +21,28 @@ class Solution {
         return c;
     }
 }
+//dfs 
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        if(grid.length==0 || grid==null) return 0;
+        
+        for( int i=0;i<grid.length;i++){
+            for( int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1)
+                return dfs(i,j,grid);            
+            }
+        }
+        return 0;
+    }
+    int dfs(int i, int j, int[][]grid){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0) return 1;
+        if(grid[i][j]==-1) return 0;
+        int c=0;
+        grid[i][j]=-1;
+        c+=dfs(i-1,j,grid);
+        c+=dfs(i,j-1,grid);
+        c+=dfs(i+1,j,grid);
+        c+=dfs(i,j+1,grid);
+        return c;
+    }
+}
